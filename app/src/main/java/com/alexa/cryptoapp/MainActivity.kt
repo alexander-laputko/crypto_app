5ce45ccdff2bda820cfb2acc3234418e77272329
alexander.laputko <alexander.laputko@ya.ru>
2022-09-27 11:15:14 +0300

    Get some data and create LazyColumn

--- app/src/main/java/com/alexa/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/alexa/cryptoapp/MainActivity.kt
@@ -3,12 +3,13 @@ package com.alexa.cryptoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -17,13 +18,26 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.alexa.cryptoapp.api.RetrofitClient
 import com.alexa.cryptoapp.model.CoinItem
 import com.alexa.cryptoapp.ui.theme.CryptoAppTheme
+import com.alexa.cryptoapp.viewmodel.CoinViewModel
 
 class MainActivity : ComponentActivity() {
+
+    val viewModel by viewModels<CoinViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             CryptoAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -31,7 +45,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    CoinsList(coinsList = viewModel.eurCoinsListResponse)
+                    viewModel.getCoinsList()
                 }
             }
         }
@@ -39,7 +54,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ItemRow(item: CoinItem) {
+fun Coin(coin: CoinItem) {
+    Row(Modifier.fillMaxWidth()) {
+        Image(
+            painter = rememberImagePainter(data = coin.image,
+                builder = {
+                placeholder(coil.base.R.drawable.notification_action_background)
+                transformations(CircleCropTransformation())
+            }),
+            contentDescription = null,
+            modifier = Modifier.weight(1f)
+        )
+        Column(
+            modifier = Modifier
+                .height(80.dp)
+                .weight(2f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = coin.name, fontSize = 16.sp)
+            Text(text = coin.symbol.toUpperCase(), color = Color.LightGray, fontSize = 14.sp)
+        }
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier
+                .height(80.dp)
+                .weight(2f),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "$ ${coin.current_price}", fontSize = 16.sp)
+            Text(
+                text = "${coin.market_cap_change_percentage_24h} %",
+                color = Color.Green,
+                fontSize = 14.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun CoinsList(coinsList: List<CoinItem>) {
+    LazyColumn {
+        itemsIndexed(items = coinsList) { index, item ->
+            Coin(coin = item)
+        }
+    }
 
 }
 
